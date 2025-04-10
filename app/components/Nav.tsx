6aaeb600aba938b85921b6a1c091b3646920108a
ewwmonya <savvymallen85@gmail.com>
2025-04-10 09:37:34 -0400

    upload

---
+++ app/components/Nav.tsx
@@ -0,0 +1,131 @@
+"use client"
+import { useRef, useState } from "react";
+import { FiLogIn } from "react-icons/fi";
+import { GrLinkNext } from "react-icons/gr";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { TiShoppingCart } from "react-icons/ti";
+
+const Nav = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const navHeight = useRef(null);
+  const linksRef = useRef(null)
+
+  const toggleShowLinks = () => {
+    setShowLinks(!showLinks)
+  };
+
+  const linkStyle = {
+    height: showLinks 
+    ? `${linksRef.current.getBoundingClientRect().height}px` : '0px'
+
+  }
+  
+
+
+  return (
+    <header className='bg-green-950 px-6 py-8 border-b'>
+      <div className='hidden lg:block'>
+        <div className=' flex justify-between gap-4 items-center'>
+          {/* Logo */}
+          <h1 className='flex flex-col text-5xl'>
+            <span className='text-lime-100'>DeLuca</span>{" "}
+            <span className=' text-yellow-300'>Hair</span>
+          </h1>
+          {/* Links */}
+
+          <div className='flex justify-center gap-x-6'>
+            <a href='?#' className='text-lime-100'>
+              Home
+            </a>
+            <a href='?#' className='text-lime-100'>
+              Services
+            </a>
+            <a href='?#' className='text-lime-100'>
+              Shop Merch
+            </a>
+            <a href='?#' className='text-lime-100'>
+              Blog
+            </a>
+            <a href='?#' className='text-lime-100'>
+              About Us
+            </a>
+          </div>
+          <div className='flex justify-between items-center gap-8'>
+            <button className='text-white flex items-center gap-1 bg-green-900 hover:bg-green-800 hover:scale-105 py-1 px-3 rounded-sm duration-500'>
+              Login <FiLogIn />{" "}
+            </button>
+
+            <button className='text-green-200'>
+              <TiShoppingCart className='text-3xl hover:text-green-100 duration-300 hover:scale-105' />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className='lg:hidden block'>
+        <div className='flex justify-between'>
+          {/* Logo */}
+          <h1 className='flex flex-col text-5xl'>
+            <span className='text-lime-100'>DeLuca</span>{" "}
+            <span className=' text-yellow-300'>Hair</span>
+          </h1>
+          <div className='flex justify-between gap-x-3 items-center'>
+            <button>
+              <TiShoppingCart className='text-white text-3xl hover:scale-105 duration-500' />
+            </button>
+            <button>
+              <RxHamburgerMenu className='text-white text-3xl' onClick={()=>{toggleShowLinks()}}/>
+            </button>
+          </div>
+        </div>
+        <div className={`text-white my-5 tracking-wider font-bold overflow-hidden transition-all duration-500 ease-in-out ${
+    showLinks ? 'opacity-100 visible' : 'opacity-0 invisible'
+  }`} ref={navHeight} style={linkStyle}>
+          <ul className='' ref={linksRef}>
+            <li>
+              <a
+                href='?#'
+                className='text-lime-100 hover:text-lime-950 hover:bg-lime-100 duration-300 flex justify-between items-center p-5'
+              >
+                Home <GrLinkNext className='text-green-950 font-bold' />
+              </a>
+            </li>
+            <li>
+              <a
+                href='?#'
+                className='text-lime-100 hover:text-lime-950 hover:bg-lime-100 duration-300 flex justify-between items-center p-5'
+              >
+                Services <GrLinkNext className='text-green-950 font-bold' />
+              </a>
+            </li>
+            <li>
+              <a
+                href='?#'
+                className='text-lime-100 hover:text-lime-950 hover:bg-lime-100 duration-300 flex justify-between items-center p-5'
+              >
+                Shop Merch <GrLinkNext className='text-green-950 font-bold' />
+              </a>
+            </li>
+            <li>
+              <a
+                href='?#'
+                className='text-lime-100 hover:text-lime-950 hover:bg-lime-100 duration-300 flex justify-between items-center p-5'
+              >
+                Blog <GrLinkNext className='text-green-950 font-bold' />
+              </a>
+            </li>
+            <li>
+              <a
+                href='?#'
+                className='text-lime-100 hover:text-lime-950 hover:bg-lime-100 duration-300 flex justify-between items-center p-5'
+              >
+                About Us <GrLinkNext className='text-green-950 font-bold' />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
+export default Nav;
