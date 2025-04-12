d739350eaef8cb35aa4f0777d4a2e7084e5afdf5
ewwmonya <savvymallen85@gmail.com>
2025-04-12 15:24:59 -0400

    Hero critter fix

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,11 +1,12 @@
 
 const Hero = () => {
   return (
-<section className="relative bg-cover bg-center h-[90vh] text-white px-6 flex items-center justify-center" style={{backgroundImage: `url('./heroImgNew.jpeg')`}}>
+<section className="relative bg-cover bg-center h-[90vh] text-white px-6 grid grid-cols-2" style={{backgroundImage: `url('./heroImgNewMin.jpg')`}}>
+
         <div className="flex items-center mx-0 max-w-6xl min-h-48">
 
-            <div className="text-center">
-                <h2 className="text-4xl font-bold tracking-wide mb-4 ">Natural Beauty. Feel Bold</h2>
+             <div className="text-center"> {/*  Text */}
+                <h2 className="text-4xl text-emerald-700  font-bold tracking-wide mb-4 ">Natural Beauty. Feel Bold</h2>
                 <p className="capitalize text-lime-100 text-2xl mb-6 font-sans tracking-widest">hair that empowers. Style the Shine</p>
                 <button className="inline-flex items-center justify-center px-5 py-3 font-sans font-semibold tracking-wide text-white bg-emerald-700 hover:bg-green-950 hover:border-emerald-700 rounded-lg hover:px-4 border-2 border-green-950 duration-500">
                           Book Now
