04d17e2938d6267639a75da4c0ca67f10e4ac807
ewwmonya <savvymallen85@gmail.com>
2025-04-14 02:41:14 -0400

    404 update

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+    return (
+      <main className=" min-h-screen  grid flex-col items-center justify-normal md:justify-center bg-green-950 text-white text-center p-6">
+        <h1 className="text-5xl font-bold mb-4">🚧 Coming Soon</h1>
+        <p className="text-lg opacity-50">This page is being styled as we speak. Check back soon!</p>
+        <p className="max-w-[400px] opacity-50 ">
+        </p>
+        
+      </main>
+    );
+  }
+  
